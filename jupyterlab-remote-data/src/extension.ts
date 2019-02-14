7b3a7cdcbd12d8ced9aecd608ef18529e4d589cc
Ian Rose <ian.r.rose@gmail.com>
2019-02-14 12:44:08 -0800

    Add event listener to filebrowser to hijack browser clicks and open datasets with the right factory.

--- jupyterlab-remote-data/src/extension.ts
+++ jupyterlab-remote-data/src/extension.ts
@@ -7,6 +7,9 @@ import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
 
 import { IDocumentManager } from '@jupyterlab/docmanager';
 
+const FACTORY = 'Remote Data';
+const DATA_MIME = 'application/vnd.jupyter.dataset+json';
+
 /**
  * The JupyterLab plugin for the GitHub Filebrowser.
  */
@@ -25,7 +28,37 @@ function activateMonkeyPatch(
   manager: IDocumentManager,
   factory: IFileBrowserFactory
 ): void {
-  console.log('Activated');
+  const browser = factory.defaultBrowser;
+  const handleEvent = async (evt: Event): Promise<void> => {
+    const event = evt as MouseEvent;
+    // Do nothing if it's not a left mouse press.
+    if (event.button !== 0) {
+      return;
+    }
+
+    // Do nothing if any modifier keys are pressed.
+    if (event.ctrlKey || event.shiftKey || event.altKey || event.metaKey) {
+      return;
+    }
+
+    // Stop the event propagation.
+    event.preventDefault();
+    event.stopPropagation();
+
+    const item = browser.modelForClick(event);
+    console.log(item);
+    if (!item) {
+      return;
+    }
+    if (item.type === 'directory') {
+      browser.model.cd(item.name).catch(error => console.error(error));
+    } else {
+      const factory = item.mimetype === DATA_MIME ? FACTORY : 'default';
+      manager.openOrReveal(item.path, factory);
+    }
+  };
+
+  browser.node.addEventListener('dblclick', handleEvent, true);
 }
 
 export default browserMonkeyPatch;
